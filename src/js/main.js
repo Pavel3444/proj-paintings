baadfe0e6fe0a3b44d6b0c5ae502cf1e49175cda
Pavel3444 <burikov.pavel3444@yandex.ru>
2020-08-15 18:16:11 +0400

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -8,4 +9,5 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horisontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item',  'vertical');
+    forms();
 });
---
+++ src/js/modules/forms.js
@@ -0,0 +1,105 @@
+// import checkNumInputs from "./checkNumInputs";
+//ddfs
+const forms = (state) => {
+
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        upload = document.querySelectorAll('[name="upload"]'),
+        message = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Данные отправлены',
+            failure: 'Что-то  пошло не так...',
+            spinner: 'assets/img/spinner.gif',
+            ok: 'assets/img/ok.png',
+            fail: 'assets/img/fail.png'
+        },
+        path = {
+            designer: 'assets/server.php',
+            question: 'assets/question.php'
+        };
+
+    // checkNumInputs('input[name="user_phone"]');
+
+    const postData = async (url, data) => {
+        // document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = "";
+        });
+        upload.forEach(item => {
+           item.previousElementSibling.textContent = "Файл не выбран";
+        });
+    };
+
+    upload.forEach(item => {
+        item.addEventListener('input', () => {
+           let dots;
+           const arr =  item.files[0].name.split('.');
+            arr[0].length > 5 ? dots = '...' : dots = '.';
+           const name = arr[0].substring(0,6) + dots + arr[1];
+           item.previousElementSibling.textContent = name;
+        })
+    });
+
+    form.forEach(item => {
+        item.addEventListener('submit', e => {
+            e.preventDefault();
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.parentNode.appendChild(statusMessage);
+
+            item.classList.add('animated', 'fadeOutUp');
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 400);
+
+            let statusImg = document.createElement('img');
+            statusImg.setAttribute('src', message.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
+            statusMessage.appendChild(statusImg);
+
+            let textMessage = document.createElement('div');
+            textMessage.textContent = message.loading;
+            statusMessage.appendChild(textMessage);
+
+
+            const formData = new FormData(item);
+
+            let api;
+            item.closest('.popup-design') || item.classList.contains('calc_form') ? api = path.designer : api = path.question;
+            console.log(api);
+
+            postData(api, formData)
+                .then(res => {
+                    console.log(res);
+                    statusImg.setAttribute('src', message.ok);
+                    textMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    textMessage.textContent = message.failure;
+                    statusImg.setAttribute('src', message.fail);
+
+                })
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                        item.style.display = "block";
+                        item.classList.remove('fadeOutUp');
+                        item.classList.add('fadeInUp');
+                    }, 5000)
+                })
+
+        })
+    })
+
+};
+
+export default forms;
