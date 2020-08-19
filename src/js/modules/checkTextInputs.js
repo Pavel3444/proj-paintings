652ca561ea805997b1c1de239e073db30b8cb8b3
Pavel3444 <burikov.pavel3444@yandex.ru>
2020-08-19 13:20:46 +0400

    add validate

---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,15 @@
+const checkTextInputs = (selector) => {
+
+    const txtInputs = document.querySelectorAll(selector);
+
+    txtInputs.forEach(input => {
+        input.addEventListener('keypress', function (e) {
+            if (e.key.match(/[^а-яё 0-9]/ig)) {
+                e.preventDefault();
+            }
+        })
+    })
+
+};
+
+export default checkTextInputs;
