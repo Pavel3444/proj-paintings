4b5bf9863211d3d12185f85a97479dcd53daf69c
Pavel3444 <burikov.pavel3444@yandex.ru>
2020-09-11 13:59:09 +0400

    add show-hide burger

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordeon from "./modules/accordeon";
+import burger from "./modules/burger";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -25,4 +26,5 @@ window.addEventListener('DOMContentLoaded', () => {
     filter();
     pictureSize('.sizes-block');
     accordeon('.accordion-heading');
+    burger('.burger-menu', '.burger')
 });
