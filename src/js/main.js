8b96ed004841a3c4eddb08877191b9910aca60e4
Pavel3444 <burikov.pavel3444@yandex.ru>
2020-09-13 23:40:09 +0400

    add pure js scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordeon from "./modules/accordeon";
 import burger from "./modules/burger";
+import scrolling from "./modules/scrolling";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -26,5 +27,6 @@ window.addEventListener('DOMContentLoaded', () => {
     filter();
     pictureSize('.sizes-block');
     accordeon('.accordion-heading');
-    burger('.burger-menu', '.burger')
+    burger('.burger-menu', '.burger');
+    scrolling('.pageup');
 });
